c09e2de0f8c20788744452b02d80367623191ef6
Sam Krishna <sam.krishna@gmail.com>
2021-11-27 11:40:35 -0800

    Adding framework header

---
+++ SKPegKit/SKPegKit.h
@@ -0,0 +1,20 @@
+//
+//  SKPegKit.h
+//  SKPegKit
+//
+//  Created by Sam Krishna on 11/27/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SKPegKit.
+FOUNDATION_EXPORT double SKPegKitVersionNumber;
+
+//! Project version string for SKPegKit.
+FOUNDATION_EXPORT const unsigned char SKPegKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SKPegKit/PublicHeader.h>
+
+// io
+#import <SKPegKit/PKTypes.h>
+#import <SKPegKit/PKReader.h>
