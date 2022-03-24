e8300bfaa57b0cef71bfef6f07c86ed9f3198522
Sam Krishna <sam.krishna@gmail.com>
2022-03-24 08:22:31 -0700

    More stubbing of TDTemplateEngine

--- SKPegKit/SKPegKit/TDTemplateEngine/TDTemplateEngine.h
+++ SKPegKit/SKPegKit/TDTemplateEngine/TDTemplateEngine.h
@@ -13,6 +13,7 @@ NS_ASSUME_NONNULL_BEGIN
 @class TDTag;
 @class TDFilter;
 @class TDTemplateContext;
+@class XParser;
 
 extern NSString *const TDTemplateEngineTagEndPrefix;
 extern NSString *const TDTemplateEngineErrorDomain;
@@ -20,12 +21,39 @@ extern const NSInteger TDTemplateEngineRenderingErrorCode;
 
 @interface TDTemplateEngine : NSObject
 
+// statitc/complile-time variables go here. This is the global scope at both compile-time and render-time. persists across compiles and renders
+@property (nonatomic, readonly, strong) TDTemplateContext *staticContext;
+
+@property (nonatomic, readonly, strong) NSString *printStartDelimiter;
+@property (nonatomic, readonly, strong) NSString *printEndDelimiter;
+@property (nonatomic, readonly, strong) NSString *tagStartDelimiter;
+@property (nonatomic, readonly, strong) NSString *tagEndDelimiter;
+
 + (instancetype)templateEngine;
 
 // pre-compile template into a tree
 - (TDNode *)compileTemplateString:(NSString *)str error:(NSError **)err;
 - (TDNode *)compileTemplateFile:(NSString *)path encoding:(NSStringEncoding)enc error:(NSError **)err;
 
+// render pre-compiled tree with render-time vars
+- (BOOL)renderTemplateTree:(TDNode *)root withVariables:(NSDictionary *)vars toStream:(NSOutputStream *)output error:(NSError **)err;
+
+// convenience. compile + render with render-time vars in one-shot
+- (BOOL)processTemplateString:(NSString *)str withVariables:(NSDictionary *)vars toStream:(NSOutputStream *)output error:(NSError **)err;
+- (BOOL)processTemplateFile:(NSString *)path encoding:(NSStringEncoding)enc withVariables:(NSDictionary *)vars toStream:(NSOutputStream *)output error:(NSError **)err;
+
+@end
+
+@interface TDTemplateEngine (TagRegistration)
+- (void)registerTagClass:(Class)cls forName:(NSString *)tagName;
+- (Class)registeredTagClassForName:(NSString *)tagName;
+- (TDFilter *)makeTagForName:(NSString *)tagName;
+@end
+
+@interface TDTemplateEngine (FilterRegistration)
+- (void)registerFilterClass:(Class)cls forName:(NSString *)filterName;
+- (Class)registeredFilterClassForName:(NSString *)filterName;
+- (TDFilter *)makeFilterForName:(NSString *)filterName;
 @end
 
 NS_ASSUME_NONNULL_END
--- SKPegKit/SKPegKit/TDTemplateEngine/TDTemplateEngine.m
+++ SKPegKit/SKPegKit/TDTemplateEngine/TDTemplateEngine.m
@@ -7,6 +7,19 @@
 
 #import "TDTemplateEngine.h"
 
+NSString *const TDTemplateEngineTagEndPrefix = @"/";
+NSString *const TDTemplateEngineErrorDomain = @"TDTemplateEngineErrorDomain";
+const NSInteger TDTemplateEngineRenderingErrorCode = 1;
+
+@interface TDTemplateEngine ()
+@property (nonatomic, readwrite, strong) NSRegularExpression *delimiterRegex;
+@property (nonatomic, readwrite, strong) NSRegularExpression *cleanerRegex;
+@property (nonatomic, readwrite, strong) TDTemplateContext *staticContext;
+@property (nonatomic, readwrite, strong) NSMutableDictionary *tagTab;
+@property (nonatomic, readwrite, strong) NSMutableDictionary *filterTab;
+@property (nonatomic, readwrite, strong) XParser *expressionParser;
+@end
+
 @implementation TDTemplateEngine
 
 + (instancetype)templateEngine {
@@ -21,5 +34,46 @@
 - (TDNode *)compileTemplateFile:(NSString *)path encoding:(NSStringEncoding)enc error:(NSError **)err {
     return nil;
 }
+
+- (BOOL)renderTemplateTree:(TDNode *)root withVariables:(NSDictionary *)vars toStream:(NSOutputStream *)output error:(NSError **)err {
+    return NO;
+}
+
+- (BOOL)processTemplateString:(NSString *)str withVariables:(NSDictionary *)vars toStream:(NSOutputStream *)output error:(NSError **)err {
+    return NO;
+}
+
+- (BOOL)processTemplateFile:(NSString *)path encoding:(NSStringEncoding)enc withVariables:(NSDictionary *)vars toStream:(NSOutputStream *)output error:(NSError **)err {
+    return NO;
+}
+
+#pragma mark - TagRegistration
+
+- (void)registerTagClass:(Class)cls forName:(NSString *)tagName {
+
+}
+
+- (Class)registeredTagClassForName:(NSString *)tagName {
+    return Nil;
+}
+
+- (TDFilter *)makeTagForName:(NSString *)tagName {
+    return nil;
+}
+
+#pragma mark - Filter Registration
+
+- (void)registerFilterClass:(Class)cls forName:(NSString *)filterName {
+
+}
+
+- (Class)registeredFilterClassForName:(NSString *)filterName {
+    return Nil;
+}
+
+- (TDFilter *)makeFilterForName:(NSString *)filterName {
+    return nil;
+}
+
 
 @end
