7d38ddf35115a17c4242a230a7fd48c3a762fb7c
Sam Krishna <sam.krishna@gmail.com>
2022-03-24 07:26:36 -0700

    Nuked all directories and am starting over.

--- Podfile
+++
@@ -1,7 +0,0 @@
-source 'https://github.com/CocoaPods/Specs.git'
-platform :osx, '10.14'
-
-target 'SKPegKitTests' do
-    pod 'OCMock', '2.2.4'
-end
-
--- Pods/Target Support Files/Pods-SKPegKitTests/Pods-SKPegKitTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SKPegKitTests : NSObject
-@end
-@implementation PodsDummy_Pods_SKPegKitTests
-@end
