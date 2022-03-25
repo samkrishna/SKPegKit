ec43013e261f0c6c668ab836a5fb2cc336680d49
Sam Krishna <sam.krishna@gmail.com>
2022-03-25 08:16:47 -0700

    Got FIRST test cases runnning! (PKReaderTests) WOOT WOOT!

---
+++ SKPegKit/SKPegKitTests/PEGKit Tests/PKReaderTests.m
@@ -0,0 +1,72 @@
+//
+//  PKReaderTests.m
+//  SKPegKitTests
+//
+//  Created by Sam Krishna on 3/25/22.
+//
+
+@import XCTest;
+#import "TDTestScaffold.h"
+#import "PKReader.h"
+
+@interface PKReaderTests : XCTestCase {
+    PKReader *reader;
+    NSString *string;
+}
+
+@end
+
+@implementation PKReaderTests
+
+- (void)setUp {
+    string = @"abcdefghijklmnopqrstuvwxyz";
+    reader = [[PKReader alloc] initWithString:string];
+}
+
+#pragma mark -
+
+- (void)testReadCharsMatch {
+    TDNotNil(reader);
+    PKUniChar c;
+
+    for (NSUInteger i = 0; i < string.length; i++) {
+        c = [string characterAtIndex:i];
+        TDEquals(c, [reader read]);
+    }
+}
+
+- (void)testReadTooFar {
+    for (NSUInteger i = 0; i < string.length; i++) {
+        [reader read];
+    }
+
+    TDEquals(PKEOF, [reader read]);
+}
+
+- (void)testUnread {
+    [reader read];
+    [reader unread];
+    PKUniChar a = 'a';
+    TDEquals(a, [reader read]);
+
+    [reader read];
+    [reader read];
+    [reader unread];
+    PKUniChar c = 'c';
+    TDEquals(c, [reader read]);
+}
+
+- (void)testUnreadTooFar {
+    [reader unread];
+    PKUniChar a = 'a';
+    TDEquals(a, [reader read]);
+
+    [reader unread];
+    [reader unread];
+    [reader unread];
+    [reader unread];
+    PKUniChar a2 = 'a';
+    TDEquals(a2, [reader read]);
+}
+
+@end
