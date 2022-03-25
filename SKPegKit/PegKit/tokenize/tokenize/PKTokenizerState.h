f58ddac9d273283092dbae3c5391b4bd72fbbf49
Sam Krishna <sam.krishna@gmail.com>
2022-03-24 21:14:39 -0700

    Add PKTokenizerState

---
+++ SKPegKit/PegKit/tokenize/tokenize/PKTokenizerState.h
@@ -0,0 +1,13 @@
+//
+//  PKTokenizerState.h
+//  SKPegKit
+//
+//  Created by Sam Krishna on 3/24/22.
+//
+
+@import Foundation;
+#import "PKTypes.h"
+
+@interface PKTokenizerState : NSObject
+
+@end
---
+++ SKPegKit/PegKit/tokenize/tokenize/PKTokenizerState.m
@@ -0,0 +1,12 @@
+//
+//  PKTokenizerState.m
+//  SKPegKit
+//
+//  Created by Sam Krishna on 3/24/22.
+//
+
+#import "PKTokenizerState.h"
+
+@implementation PKTokenizerState
+
+@end
