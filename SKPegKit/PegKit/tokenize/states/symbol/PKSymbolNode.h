56054e9730a7384c3785815d312d549438148ce2
Sam Krishna <sam.krishna@gmail.com>
2022-03-28 18:58:48 -0700

    Fill out PKSymbolNode

--- SKPegKit/PegKit/tokenize/states/symbol/PKSymbolNode.h
+++ SKPegKit/PegKit/tokenize/states/symbol/PKSymbolNode.h
@@ -6,7 +6,14 @@
 //
 
 @import Foundation;
+#import <SKPegKit/PKTypes.h>
 
 @interface PKSymbolNode : NSObject
+
+- (instancetype)initWithParent:(PKSymbolNode *)p character:(PKUniChar)c;
+
+@property (nonatomic, readonly, strong) NSString *ancestor;
+
+@property (nonatomic, readwrite, assign) BOOL reportsAddedSymbolsOnly;
 
 @end
--- SKPegKit/PegKit/tokenize/states/symbol/PKSymbolNode.m
+++ SKPegKit/PegKit/tokenize/states/symbol/PKSymbolNode.m
@@ -7,6 +7,47 @@
 
 #import "PKSymbolNode.h"
 
+@interface PKSymbolNode ()
+@property (nonatomic, readwrite, strong) NSString *ancestry;
+@property (nonatomic, readwrite, strong) PKSymbolNode *parent;
+@property (nonatomic, readwrite, strong) NSMutableDictionary *children;
+@property (nonatomic, readwrite, strong) NSString *string;
+
+@property (nonatomic, readwrite, assign) PKUniChar character;
+@end
+
 @implementation PKSymbolNode
+
+- (instancetype)initWithParent:(PKSymbolNode *)p character:(PKUniChar)c {
+    if (!(self = [super init])) { return nil; }
+
+    self.parent = p;
+    self.character = c;
+    self.children = [NSMutableDictionary dictionary];
+    self.string = [NSString stringWithFormat:@"%C", (unichar)self.character];
+    [self determineAncestry];
+
+    return self;
+}
+
+- (void)determineAncestry {
+    if (PKEOF == self.parent.character) {
+        self.ancestry = self.string;
+    }
+    else {
+        NSMutableString *result = [NSMutableString string];
+        PKSymbolNode *n = self;
+        while (PKEOF != n.character) {
+            [result insertString:n.string atIndex:0];
+            n = n.parent;
+        }
+
+        self.ancestry = result; // optimization
+    }
+}
+
+- (NSString *)description {
+    return [NSString stringWithFormat:@"<PKSymbolNode %@>", self.ancestry];
+}
 
 @end
