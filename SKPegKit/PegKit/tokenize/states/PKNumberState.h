498032af1bb353190666c205ffd9a0cb54a2f7c8
Sam Krishna <sam.krishna@gmail.com>
2022-03-29 08:47:17 -0700

    Stubbed out PKNumberState

--- SKPegKit/PegKit/tokenize/states/PKNumberState.h
+++ SKPegKit/PegKit/tokenize/states/PKNumberState.h
@@ -5,8 +5,26 @@
 //  Created by Sam Krishna on 3/25/22.
 //
 
-#import <SKPegKit/SKPegKit.h>
+#import <SKPegKit/PKTokenizerState.h>
+
+@class PKSymbolRootNode;
 
 @interface PKNumberState : PKTokenizerState
+
+- (void)addPrefix:(NSString *)s forRadix:(NSUInteger)r;
+- (void)removePrefix:(NSString *)s;
+
+- (void)addSuffix:(NSString *)s forRadix:(NSUInteger)r;
+- (void)removeSuffix:(NSString *)s;
+
+- (void)addGroupingSeparator:(PKUniChar)c forRadix:(NSUInteger)r;
+- (void)removeGroupingSeparator:(PKUniChar)c;
+
+@property (nonatomic, readwrite, assign) BOOL allowsTrailingDecimalSeparator;
+@property (nonatomic, readwrite, assign) BOOL allowsScientificNotation;
+@property (nonatomic, readwrite, assign) BOOL allowsFloatingPoint;
+@property (nonatomic, readwrite, assign) PKUniChar positivePrefix;
+@property (nonatomic, readwrite, assign) PKUniChar negativePrefix;
+@property (nonatomic, readwrite, assign) PKUniChar decimalSeparator;
 
 @end
--- SKPegKit/PegKit/tokenize/states/PKNumberState.m
+++ SKPegKit/PegKit/tokenize/states/PKNumberState.m
@@ -5,8 +5,38 @@
 //  Created by Sam Krishna on 3/25/22.
 //
 
+#import "PKReader.h"
+#import "PKToken.h"
+#import "PKTokenizer.h"
+#import "PKSymbolState.h"
+#import "PKWhitespaceState.h"
+#import "PKSymbolRootNode.h"
 #import "PKNumberState.h"
 
 @implementation PKNumberState
+
+- (void)addPrefix:(NSString *)s forRadix:(NSUInteger)r {
+
+}
+
+- (void)removePrefix:(NSString *)s {
+
+}
+
+- (void)addSuffix:(NSString *)s forRadix:(NSUInteger)r {
+
+}
+
+- (void)removeSuffix:(NSString *)s {
+
+}
+
+- (void)addGroupingSeparator:(PKUniChar)c forRadix:(NSUInteger)r {
+
+}
+
+- (void)removeGroupingSeparator:(PKUniChar)c {
+
+}
 
 @end
