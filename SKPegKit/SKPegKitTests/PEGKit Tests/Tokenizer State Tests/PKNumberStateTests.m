518aa703bc285e15a81c303cc9b50c7f88889f01
Sam Krishna <sam.krishna@gmail.com>
2022-03-29 15:44:44 -0700

    Initial batch of PKNumberStateTests that test octal number reading

--- SKPegKit/SKPegKitTests/PEGKit Tests/Tokenizer State Tests/PKNumberStateTests.m
+++ SKPegKit/SKPegKitTests/PEGKit Tests/Tokenizer State Tests/PKNumberStateTests.m
@@ -27,11 +27,41 @@
 
 - (void)testOctPrefix0 {
     [t.numberState addPrefix:@"0" forRadix:8];
-    s= @"0";
+    s = @"0";
     t.string = s;
     PKToken *tok = [t nextToken];
     TDTrue(tok.isNumber);
     TDEqualObjects(@"0", tok.stringValue);
 }
+
+- (void)testOctPrefixNeg0 {
+    [t.numberState addPrefix:@"0" forRadix:8];
+    s = @"-0";
+    t.string = s;
+    PKToken *tok = [t nextToken];
+    TDTrue(tok.isNumber);
+    TDEqualObjects(@"-0", tok.stringValue);
+}
+
+- (void)testOctPrefix01 {
+    [t.numberState addPrefix:@"0" forRadix:8];
+    s = @"01";
+    t.string = s;
+    PKToken *tok = [t nextToken];
+    TDEquals(1.0, tok.doubleValue);
+    TDTrue(tok.isNumber);
+    TDEqualObjects(@"01", tok.stringValue);
+}
+
+- (void)testOctPrefix010 {
+    [t.numberState addPrefix:@"0" forRadix:8];
+    s = @"010";
+    t.string = s;
+    PKToken *tok = [t nextToken];
+    TDEquals(8.0, tok.doubleValue);
+    TDTrue(tok.isNumber);
+    TDEqualObjects(@"010", tok.stringValue);
+}
+
 
 @end
