a26bff0398145f31bd0d37ae0bdd74c5a0114297
Sam Krishna <sam.krishna@gmail.com>
2022-03-28 19:47:34 -0700

    Filled out PKWhitespaceState

--- SKPegKit/PegKit/tokenize/states/PKWhitespaceState.h
+++ SKPegKit/PegKit/tokenize/states/PKWhitespaceState.h
@@ -5,9 +5,13 @@
 //  Created by Sam Krishna on 3/25/22.
 //
 
-#import <SKPegKit/SKPegKit.h>
+#import <SKPegKit/PKTokenizerState.h>
 
 @interface PKWhitespaceState : PKTokenizerState
 
+- (BOOL)isWhitespaceChar:(PKUniChar)c;
+- (void)setWhitespaceChars:(BOOL)yn from:(PKUniChar)start to:(PKUniChar)end;
+
+@property (nonatomic, readwrite, assign) BOOL reportsWhitespaceTokens;
 @end
 
--- SKPegKit/PegKit/tokenize/states/PKWhitespaceState.m
+++ SKPegKit/PegKit/tokenize/states/PKWhitespaceState.m
@@ -5,8 +5,101 @@
 //  Created by Sam Krishna on 3/25/22.
 //
 
+#import "PKReader.h"
+#import "PKTokenizer.h"
+#import "PKToken.h"
+#import <SKPegKit/PKTypes.h>
 #import "PKWhitespaceState.h"
 
+#define PKTRUE (id)kCFBooleanTrue
+#define PKFALSE (id)kCFBooleanFalse
+
+@interface PKToken ()
+@property (nonatomic, readwrite, assign) NSUInteger offset;
+@end
+
+@interface PKTokenizer ()
+@property (nonatomic, readwrite, assign) NSUInteger lineNumber;
+@end
+
+@interface PKTokenizerState ()
+- (void)resetWithReader:(PKReader *)r;
+- (void)append:(PKUniChar)c;
+
+@property (nonatomic, readwrite, strong) NSString *bufferedString;
+@property (nonatomic, readwrite, assign) NSUInteger offset;
+@end
+
+@interface PKWhitespaceState ()
+@property (nonatomic, readwrite, strong) NSMutableArray *whitespaceChars;
+@end
+
 @implementation PKWhitespaceState
+
+- (instancetype)init {
+    if (!(self = [super init])) { return nil; }
+
+    const NSUInteger len = 255;
+    self.whitespaceChars = [NSMutableArray arrayWithCapacity:len];
+
+    for (NSUInteger i = 0; i <= len; i++) {
+        [self.whitespaceChars addObject:PKFALSE];
+    }
+
+    [self setWhitespaceChars:YES from:0 to:' '];
+
+    return self;
+}
+
+- (void)setWhitespaceChars:(BOOL)yn from:(PKUniChar)start to:(PKUniChar)end {
+    NSUInteger len = self.whitespaceChars.count;
+
+    if (start > len || end > len || start < 0 || end < 0) {
+        [NSException raise:@"PKWhitespaceStateNotSupportedException" format:@"PKWhitespaceState only supports setting word chars for chars in the latin1 set (under 256)"];
+    }
+
+    id obj = yn ? PKTRUE : PKFALSE;
+    for (NSUInteger i = start; i <= end; i++) {
+        [self.whitespaceChars replaceObjectAtIndex:i withObject:obj];
+    }
+}
+
+- (BOOL)isWhitespaceChar:(PKUniChar)cin {
+    if (cin < 0 || cin > (self.whitespaceChars.count - 1)) {
+        return NO;
+    }
+
+    return (PKTRUE == self.whitespaceChars[cin]);
+}
+
+- (PKToken *)nextTokenFromReader:(PKReader *)r startingWith:(PKUniChar)cin tokenizer:(PKTokenizer *)t {
+    NSParameterAssert(r);
+    if (self.reportsWhitespaceTokens) {
+        [self resetWithReader:r];
+    }
+
+    PKUniChar c = cin;
+    while ([self isWhitespaceChar:c]) {
+        if ('\n' == c) {
+            t.lineNumber++;
+        }
+        if (self.reportsWhitespaceTokens) {
+            [self append:c];
+        }
+        c = [r read];
+    }
+
+    if (PKEOF != c) {
+        [r unread];
+    }
+
+    if (self.reportsWhitespaceTokens) {
+        PKToken *tok = [PKToken tokenWithTokenType:PKTokenTypeWhitespace stringValue:self.bufferedString doubleValue:0.0];
+        tok.offset = self.offset;
+        return tok;
+    }
+
+    return [t nextToken];
+}
 
 @end
