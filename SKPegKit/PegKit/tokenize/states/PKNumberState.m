833e461eab41e802868c8109fcb085f8f88c85c0
Sam Krishna <sam.krishna@gmail.com>
2022-03-29 15:43:27 -0700

    Added internal declarations for PKNumberState

--- SKPegKit/PegKit/tokenize/states/PKNumberState.m
+++ SKPegKit/PegKit/tokenize/states/PKNumberState.m
@@ -13,10 +13,29 @@
 #import "PKSymbolRootNode.h"
 #import "PKNumberState.h"
 
-@implementation PKNumberState
+@interface PKNumberState ()
+@property (nonatomic, readwrite, strong) PKSymbolRootNode *prefixRootNode;
+@property (nonatomic, readwrite, strong) PKSymbolRootNode *suffixRootNode;
+@property (nonatomic, readwrite, strong) NSMutableDictionary *radixForPrefix;
+@property (nonatomic, readwrite, strong) NSMutableDictionary *radixForSuffix;
+@property (nonatomic, readwrite, strong) NSMutableDictionary *separatorsForRadix;
 
-- (void)addPrefix:(NSString *)s forRadix:(NSUInteger)r {
+@property (nonatomic, readwrite, strong) NSString *prefix;
+@property (nonatomic, readwrite, strong) NSString *suffix;
+@property (nonatomic, readonly, assign) double value;
+@end
 
+@implementation PKNumberState {
+    BOOL _isFraction;
+    BOOL _isNegative;
+    BOOL _gotADigit;
+    NSUInteger _base;
+    PKUniChar _originalCin;
+    PKUniChar _c;
+    double _doubleValue;
+
+    NSUInteger _exp;
+    BOOL _isNegativeExp;
 }
 
 - (void)removePrefix:(NSString *)s {
