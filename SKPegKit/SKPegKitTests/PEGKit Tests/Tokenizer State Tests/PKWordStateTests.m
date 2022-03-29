719d7b16ca374275ae5f01a5444726a2f8dc0110
Sam Krishna <sam.krishna@gmail.com>
2022-03-29 08:06:57 -0700

    Filled out WordStateTests

--- SKPegKit/SKPegKitTests/PEGKit Tests/Tokenizer State Tests/PKWordStateTests.m
+++ SKPegKit/SKPegKitTests/PEGKit Tests/Tokenizer State Tests/PKWordStateTests.m
@@ -7,18 +7,98 @@
 
 #import "TDTestScaffold.h"
 
-@interface PKWordStateTests : XCTestCase
+@interface PKWordStateTests : XCTestCase {
+    PKWordState *wordState;
+    PKReader *r;
+    NSString *s;
+}
 
 @end
 
 @implementation PKWordStateTests
 
 - (void)setUp {
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    wordState = [[PKWordState alloc] init];
 }
 
-- (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
+- (void)testA {
+    s = @"a";
+    r = [[PKReader alloc] initWithString:s];
+    PKToken *tok = [wordState nextTokenFromReader:r startingWith:[r read] tokenizer:nil];
+    TDEqualObjects(@"a", tok.stringValue);
+    TDEqualObjects(@"a", tok.value);
+    TDTrue(tok.isWord);
+    TDEquals(PKEOF, [r read]);
+}
+
+- (void)testASpace {
+    s = @"a ";
+    r = [[PKReader alloc] initWithString:s];
+    PKToken *tok = [wordState nextTokenFromReader:r startingWith:[r read] tokenizer:nil];
+    TDEqualObjects(@"a", tok.stringValue);
+    TDEqualObjects(@"a", tok.value);
+    TDTrue(tok.isWord);
+    TDEquals((PKUniChar)' ', [r read]);
+}
+
+- (void)testAB {
+    s = @"ab";
+    r = [[PKReader alloc] initWithString:s];
+    PKToken *tok = [wordState nextTokenFromReader:r startingWith:[r read] tokenizer:nil];
+    TDEqualObjects(s, tok.stringValue);
+    TDEqualObjects(s, tok.value);
+    TDTrue(tok.isWord);
+    TDEquals(PKEOF, [r read]);
+}
+
+- (void)testABC {
+    s = @"abc";
+    r = [[PKReader alloc] initWithString:s];
+    PKToken *tok = [wordState nextTokenFromReader:r startingWith:[r read] tokenizer:nil];
+    TDEqualObjects(s, tok.stringValue);
+    TDEqualObjects(s, tok.value);
+    TDTrue(tok.isWord);
+    TDEquals(PKEOF, [r read]);
+}
+
+- (void)testItApostropheS {
+    s = @"it's";
+    r = [[PKReader alloc] initWithString:s];
+    PKToken *tok = [wordState nextTokenFromReader:r startingWith:[r read] tokenizer:nil];
+    TDEqualObjects(s, tok.stringValue);
+    TDEqualObjects(s, tok.value);
+    TDTrue(tok.isWord);
+    TDEquals(PKEOF, [r read]);
+}
+
+- (void)testTwentyDashFive {
+    s = @"twenty-five";
+    r = [[PKReader alloc] initWithString:s];
+    PKToken *tok = [wordState nextTokenFromReader:r startingWith:[r read] tokenizer:nil];
+    TDEqualObjects(s, tok.stringValue);
+    TDEqualObjects(s, tok.value);
+    TDTrue(tok.isWord);
+    TDEquals(PKEOF, [r read]);
+}
+
+- (void)testTwentyUnderscoreFive {
+    s = @"twenty_five";
+    r = [[PKReader alloc] initWithString:s];
+    PKToken *tok = [wordState nextTokenFromReader:r startingWith:[r read] tokenizer:nil];
+    TDEqualObjects(s, tok.stringValue);
+    TDEqualObjects(s, tok.value);
+    TDTrue(tok.isWord);
+    TDEquals(PKEOF, [r read]);
+}
+
+- (void)testNumber1 {
+    s = @"number1";
+    r = [[PKReader alloc] initWithString:s];
+    PKToken *tok = [wordState nextTokenFromReader:r startingWith:[r read] tokenizer:nil];
+    TDEqualObjects(s, tok.stringValue);
+    TDEqualObjects(s, tok.value);
+    TDTrue(tok.isWord);
+    TDEquals(PKEOF, [r read]);
 }
 
 @end
