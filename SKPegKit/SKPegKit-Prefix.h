02a76d273933dfaa9ab9189a3b89f2f39d5a7cfb
Sam Krishna <sam.krishna@gmail.com>
2022-03-28 19:00:28 -0700

    Add more asserts and defines in prefix.pch

--- SKPegKit/SKPegKit-Prefix.h
+++ SKPegKit/SKPegKit-Prefix.h
@@ -12,3 +12,9 @@
 #define TDAssert(b) NSAssert2((b), @" %s : assert(%@)", __PRETTY_FUNCTION__, @#b);
 #define TDAssertMainThread() NSAssert1([NSThread isMainThread], @"%s should be called on the main thread only.", __PRETTY_FUNCTION__);
 #define TDAssertNotMainThread() NSAssert1(![NSThread isMainThread], @"%s should be called on the main thread only.", __PRETTY_FUNCTION__);
+
+#define PKAssertMainThread() NSAssert1([NSThread isMainThread], @"%s should be called on the main thread only.", __PRETTY_FUNCTION__);
+#define PKAssertNotMainThread() NSAssert1(![NSThread isMainThread], @"%s should be called on the main thread only.", __PRETTY_FUNCTION__);
+
+#define PK_PLATFORM_EMAIL_STATE 0
+#define PK_PLATFORM_TWITTER_STATE 0
